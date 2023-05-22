9ba5dfcdeb8d6c98caeb753ee4a8deacf71dff7e
Vn0000 <workmailkonovalov@gmail.com>
2023-05-22 11:16:18 +0400

    Добавлен Data слой в котором создан обьект для хранения данных.

--- app/src/main/java/com/example/preparatlist/domain/ShopItem.kt
+++ app/src/main/java/com/example/preparatlist/domain/ShopItem.kt
@@ -1,8 +1,13 @@
 package com.example.preparatlist.domain
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val count: Int,
     val enable: Boolean,
-)
+    var id: Int = UDEFIND_ID
+) {
+
+    companion object {
+        const val UDEFIND_ID = -1
+    }
+}
